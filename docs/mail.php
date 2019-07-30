c9a43bf3799f5a0f1f591d54ff80ac83f529aedc
Kat Harris <katharrismail@gmail.com>
2019-07-30 15:34:49 -0400

    image size change

--- docs/mail.php
+++ docs/mail.php
@@ -1,8 +1,13 @@
 <?php
+if ( !empty($honeypot) ){
+    return; //you may add code here to echo an error etc.
+}else{
+ 
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-
+$honeypot = $_POST['firstname'];
 
 $message = $_POST['message'];
 $formcontent=" From: $name \n Phone: $phone \n  Email: $email \n  Message: $message";
@@ -11,4 +16,6 @@ $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!" . " -" . "<a href='http://pristinepoochies.com/' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+}
+
 ?>
